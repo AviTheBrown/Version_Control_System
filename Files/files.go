56cbe5a4b4e766d58166b0877e0b2ee29804f8fd
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2024-01-15 01:38:42 -0500

    created function that creates and returns file to FileInfo struct.

--- Files/files.go
+++ Files/files.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"log"
+	datatypes "Version_Control_System/DataTypes"
 	"os"
 )
 
-func CreateFile(filePath string) *os.File {
-	openedFile, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0644)
+func CreateFile(filePath string) (*os.File, error) {
+	createdFile := new(datatypes.FileInfo)
+	var err error
+	createdFile.File, err = os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	return openedFile
+	return createdFile.File, nil
 }
