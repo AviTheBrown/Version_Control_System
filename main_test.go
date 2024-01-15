2ce057c41907d0d9aea1996c84ec511c1e56963f
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2024-01-15 01:40:53 -0500

    added more test for file creation.

--- main_test.go
+++ main_test.go
@@ -1,9 +1,13 @@
 package main
 
-import "testing"
+import (
+	datatypes "Version_Control_System/DataTypes"
+	"os"
+	"testing"
+)
 
 func TestPrintedValues(t *testing.T) {
-	mySVCS := SVCS{
+	mySVCS := datatypes.SVCS{
 		"config":   "Get and set a username.",
 		"add":      "Add a file to the index.",
 		"log":      "Show commit logs.",
@@ -25,4 +29,22 @@ func TestPrintedValues(t *testing.T) {
 	if expectedError != actualError {
 		t.Errorf("Expected error: %q but got %q", expectedError, actualError)
 	}
+}
+
+func TestCreateUser(t *testing.T) {
+	user1 := new(datatypes.User)
+	expectedUserDefaultName := ""
+	actualUserDefaultName := user1.UserName
+
+	if expectedUserDefaultName != actualUserDefaultName {
+		t.Errorf("Expected :%q but got :%q", expectedUserDefaultName, actualUserDefaultName)
+	}
+
+	expectedUserDefaultFile := (*os.File)(nil)
+	actualUserDefaultFile := user1.FileInfo.File
+
+	if expectedUserDefaultFile != actualUserDefaultFile {
+		t.Errorf("Expected File to be : %v but got %v", expectedUserDefaultFile, actualUserDefaultFile)
+	}
+
 }
