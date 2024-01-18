0cde9e1b1476d279de98a720d6fc7f673fffaf87
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2024-01-17 22:55:03 -0500

    added file adding and file checking functions

--- DataTypes/data_types.go
+++ DataTypes/data_types.go
@@ -1,12 +1,15 @@
 package datatypes
 
 import (
+	files "Version_Control_System/Files"
 	"fmt"
+	"os"
 )
 
 type SVCS map[string]string
 
 type FileInfo struct {
+	Files     []*os.File
 	FileNames []string
 }
 type User struct {
@@ -19,7 +22,6 @@ func CreateUser() *User {
 }
 func (u *User) ConfigAction(userName string) string {
 	if u.UserName == userName {
-		//  create a temp User object with only UserName field
 		return fmt.Sprintf("The username is %s", u.UserName)
 	}
 	u.UserName = userName
@@ -35,13 +37,29 @@ func containsFile(files []string, target string) bool {
 	return false
 }
 func (u *User) AddAction(fileName string) string {
+
+	var err error
+	// create the file
+	openedFile, err := files.CreateFile(fileName)
+	if err != nil {
+		return fmt.Sprintf("Error opening file %s: %v", fileName, err)
+	}
+
+	// check if the file exist
+	_, err = os.Stat(fileName)
+	if os.IsNotExist(err) {
+		return fmt.Sprintf("Can't find '%s'.", fileName)
+	}
+
+	// check for duplicates
 	output := fmt.Sprintf("'%s'is already tracked.", fileName)
 	if containsFile(u.FileNames, fileName) {
-		for _, files := range u.FileNames {
-			fmt.Println(files)
-		}
 		return output
 	}
+
+	// add to slice of tracked files
+	u.Files = append(u.Files, openedFile)
+	// add to tracked file names
 	u.FileNames = append(u.FileNames, fileName)
 	return fmt.Sprintf("The file '%v' is tracked.", fileName)
 }
