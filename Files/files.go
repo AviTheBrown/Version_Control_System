c1821e99c46d086f5ef84344f1c69b3446680564
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2024-01-29 00:47:12 -0500

    added function that creat parent dir and child files for config and index.

--- Files/files.go
+++ Files/files.go
@@ -2,9 +2,28 @@ package files
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
+func CreateDirWithChildFiles() {
+	dirPath := "./vcs"
+	err := os.MkdirAll(dirPath, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileNames := []string{"config.tx", "index.tx"}
+	for _, filename := range fileNames {
+		filepath := dirPath + "/" + filename
+		file, err := os.Create(filepath)
+		if err != nil {
+			fmt.Println("Error creating files.")
+			log.Fatal(err)
+		}
+		defer file.Close()
+	}
+}
 func CreateFile(filePath string) (*os.File, error) {
 	createdFile, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
