0f1a13ae7d40ad81b242910c817a0888ee8c7ea2
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2024-03-07 13:38:21 -0500

    refactor: Simplify commit hash ID generation
    - Remove timestamp from hash input
    - Use sha256.New() instead of sha256.Sum256()
    - Directly write commit message bytes to hash
    - Use hex.EncodeToString() for encoding hash to string
    - Remove unnecessary fmt.Sprintf() usage
    
    The commit hash ID generation process has been simplified and optimized. Previously, the timestamp was included in the hash input, but it has been removed to make the hash ID solely dependent on the commit message. The code now uses sha256.New() to create a new hash instance, writes the commit message bytes directly to the hash, and encodes the resulting hash using hex.EncodeToString(). The unnecessary usage of fmt.Sprintf() has been removed. These changes streamline the hash ID generation process while maintaining its functionality.

--- Hashing/hashing.go
+++ Hashing/hashing.go
@@ -2,22 +2,19 @@ package hashing
 
 import (
 	"crypto/sha256"
+	"encoding/hex"
 	"errors"
-	"fmt"
-	"time"
 )
 
 func GenerateCommitHashID(commitMessage string) (string, error) {
 	if commitMessage == "" {
 		return "", errors.New("Commit messages cannot be empty.")
 	}
-	timeStamp := time.Now().Format(time.RFC3339)
-	hashInput := commitMessage + timeStamp
+	sha256Hash := sha256.New()
+	sha256Hash.Write([]byte(commitMessage))
 
-	hash := sha256.Sum256([]byte(hashInput))
-	commitID := fmt.Sprint("%x", hash)
-
-	return commitID, nil
+	hexString := hex.EncodeToString(sha256Hash.Sum(nil))
+	return hexString, nil
 }
 func HashComparison(hash1, hash2 string) bool {
 	return hash1 == hash2
