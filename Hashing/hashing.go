3f49590a1d7f88d039dfa37aef44a003bf98fc0f
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2024-03-13 20:40:05 -0400

    feat: Add file hashing functionality
    - Introduce GenerateHashForFiles function to hash multiple files
    - Use sha256 to generate hash for each file
    - Concatenate file hashes to create a cumulative hash string
    - Handle file opening and hashing errors gracefully
    - Update import statements to include necessary packages

--- Hashing/hashing.go
+++ Hashing/hashing.go
@@ -1,10 +1,35 @@
 package hashing
 
 import (
+	datatypes "Version_Control_System/DataTypes"
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
+func GenerateHashForFiles(files []string, user datatypes.User) string {
+	sha256Hash := sha256.New()
+	var fileHashString string
+	for _, file := range files {
+		file, err := os.Open(file)
+		if err != nil {
+			fmt.Println("There was a problem opening the file.")
+			log.Fatal(err)
+		}
+		if _, err := io.Copy(sha256Hash, file); err != nil {
+			fmt.Println("There was a problem hashing the file.")
+			log.Fatal()
+		}
+		sha256HashValue := sha256Hash.Sum(nil)
+		fileHashString = hex.EncodeToString(sha256HashValue)
+		user.FileInfo.FileHash = fileHashString
+
+	}
+	return fileHashString
+}
 func GenerateCommitHashID(commitMessage string) (string, error) {
 	if commitMessage == "" {
 		return "Message was not passed.", nil
