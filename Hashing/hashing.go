24ca08b973ba478b45cd46d06e4f2f37a69f4769
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2024-03-18 20:49:46 -0400

    refactor: Simplify hashing package
    - Remove unused imports: datatypes, fmt, log, os
    - Remove gernerateHashForFileMinor function
    - Remove GenerateHashForFiles function

--- Hashing/hashing.go
+++ Hashing/hashing.go
@@ -1,37 +1,33 @@
 package hashing
 
 import (
-	datatypes "Version_Control_System/DataTypes"
 	"crypto/sha256"
 	"encoding/hex"
-	"fmt"
-	"log"
-	"os"
 )
 
-func gernerateHashForFileMinor(fileData []byte) (string, error) {
-	sha256Hash := sha256.New()
-	sha256Hash.Write(fileData)
-	sha256HashValue := sha256Hash.Sum(nil)
-	filehash := hex.EncodeToString(sha256HashValue)
-	return filehash, nil
-}
-func GenerateHashForFiles(files []string, user *datatypes.User) {
-	for _, file := range files {
-		fileData, err := os.ReadFile(file)
-		if err != nil {
-			fmt.Println("There was a problem opening the file.")
-			log.Fatal(err)
-		}
+// func gernerateHashForFileMinor(fileData []byte) (string, error) {
+// 	sha256Hash := sha256.New()
+// 	sha256Hash.Write(fileData)
+// 	sha256HashValue := sha256Hash.Sum(nil)
+// 	filehash := hex.EncodeToString(sha256HashValue)
+// 	return filehash, nil
+// }
+// func GenerateHashForFiles(files []string, user *datatypes.User) {
+// 	for _, file := range files {
+// 		fileData, err := os.ReadFile(file)
+// 		if err != nil {
+// 			fmt.Println("There was a problem opening the file.")
+// 			log.Fatal(err)
+// 		}
 
-		fileHashString, err := gernerateHashForFileMinor(fileData)
-		if err != nil {
-			fmt.Println("There was a problem create the hash.")
-			log.Fatal(err)
-		}
-		user.AddFileToMeta(file, fileData, fileHashString)
-	}
-}
+//			fileHashString, err := gernerateHashForFileMinor(fileData)
+//			if err != nil {
+//				fmt.Println("There was a problem creating the hash.")
+//				log.Fatal(err)
+//			}
+//			user.AddFileToMeta(file, fileData, fileHashString)
+//		}
+//	}
 func GenerateCommitHashID(commitMessage string) (string, error) {
 	if commitMessage == "" {
 		return "Message was not passed.", nil
@@ -44,4 +40,11 @@ func GenerateCommitHashID(commitMessage string) (string, error) {
 }
 func HashComparison(hash1, hash2 string) bool {
 	return hash1 == hash2
+}
+
+func HashFileData(data []byte) string {
+	sha256Hash := sha256.New()
+	sha256Hash.Write(data)
+	hash := sha256Hash.Sum(nil)
+	return hex.EncodeToString(hash)
 }
